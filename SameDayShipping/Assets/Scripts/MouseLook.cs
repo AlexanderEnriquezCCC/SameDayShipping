2f492ad4ef95c5e059a201dc92ff36fe824051b5
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2020-12-11 13:18:28 -0600

    fixes stuff concerning Y axis pick up

--- SameDayShipping/Assets/Scripts/MouseLook.cs
+++ SameDayShipping/Assets/Scripts/MouseLook.cs
@@ -22,7 +22,7 @@ public class MouseLook : MonoBehaviour
         xRotation -= mouseY;
 
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        xRotation=  Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
