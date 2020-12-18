0bc6e14e39a93861dbfcc8127e73fee51700b379
AlenLi <shilian.li@loop.colum.edu>
2020-12-17 23:48:12 -0600

    Basic ScoreSystem Done

---
+++ SameDayShipping/Assets/Scripts/ScoreSystem/GameScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class GameScore : MonoBehaviour
+{
+    public static int Score = 0;
+   void OnGUI()
+    {
+        GUI.Box(new Rect(100, 100, 100, 100), Score.ToString());
+    }
+}
